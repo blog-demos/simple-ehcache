c9742f32844ccd619fa6b3e2c83e381434720c26
Q-WHai <return_zero0@163.com>
2019-06-19 14:53:50 +0800

    test:1.添加从磁盘读取缓存;2.添加从xml配置缓存设置项

--- src/test/java/pers/hai/simple/ehcache/EhcacheTests.java
+++ src/test/java/pers/hai/simple/ehcache/EhcacheTests.java
@@ -1,75 +1,80 @@
 package pers.hai.simple.ehcache;
 
-import org.apache.log4j.Logger;
 import org.ehcache.*;
+import org.ehcache.config.Configuration;
 import org.ehcache.config.builders.CacheConfigurationBuilder;
 import org.ehcache.config.builders.CacheManagerBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
 import org.ehcache.config.builders.UserManagedCacheBuilder;
 import org.ehcache.config.units.EntryUnit;
 import org.ehcache.config.units.MemoryUnit;
+import org.ehcache.spi.service.ServiceCreationConfiguration;
+import org.ehcache.xml.XmlConfiguration;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.URL;
 
 /**
  * TODO
  * <p>
  * Create Time: 2019-06-16 11:34
- * Last Modify: 2019-06-16
+ * Last Modify: 2019-06-19
  *
  * @author Q-WHai
  * @see <a href="https://github.com/qwhai">https://github.com/qwhai</a>
  */
 public class EhcacheTests {
 
-    private final Logger logger = Logger.getLogger(EhcacheTests.class);
-
     @Test
     public void test1() {
-        CacheManager cm = CacheManagerBuilder
+        CacheManager manager = CacheManagerBuilder
                 .newCacheManagerBuilder()
-                .withCache(
-                        "preConfigured", // 缓存别名
-                        CacheConfigurationBuilder.newCacheConfigurationBuilder(
-                                Long.class,
-                                String.class,
-                                ResourcePoolsBuilder.heap(20) // 设置缓存堆容纳元素个数
-                        )
-                )
-                .build();
-        cm.init();
-
-        // 从缓存管理器中获取预定的缓存
-        Cache<Long, String> preConfigured = cm.getCache("preConfigured", Long.class, String.class);
+                .build(true);
 
         // 直接从缓存管理器创建一个新的缓存
-        Cache<Integer, String> myCache = cm.createCache(
-                "myCache",
+        Cache<Integer, String> cache = manager.createCache(
+                "cache_1",
                 CacheConfigurationBuilder.newCacheConfigurationBuilder(
                         Integer.class,
                         String.class,
-                        ResourcePoolsBuilder.heap(20)
+                        ResourcePoolsBuilder.heap(200)
+                ).build()
+        );
+
+        Cache<Integer, String> cache2 = manager.createCache(
+                "cache_2",
+                CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                        Integer.class,
+                        String.class,
+                        ResourcePoolsBuilder.heap(200)
                 ).build()
         );
 
         // 向缓存里添加缓存键值
-        for (int i = 0; i < 20; i++) {
-            myCache.put(i, String.format("@%d", i));
-        }
-
+        for (int i = 0; i < 200; i++)
+            cache.put(i, String.format("@%d", i));
         // 从指定缓存里获取键值
-        for (int i = 0; i < 20; i++) {
-            logger.info(String.format("%d : %s", i, myCache.get(i)));
-        }
+        for (int i = 0; i < 200; i++)
+            System.out.println(String.format("[cache_1]%d : %s", i, cache.get(i)));
 
-        cm.close();
+        // 向缓存里添加缓存键值
+        for (int i = 0; i < 200; i++)
+            cache2.put(i, String.format("@%d", i));
+        // 从指定缓存里获取键值
+        for (int i = 0; i < 200; i++)
+            System.out.println(String.format("[cache_2]%d : %s", i, cache2.get(i)));
+
+        manager.close();
     }
 
     // 这里测试添加的键值数大于配置的堆大小时的情况
     @Test
     public void test2() {
-        CacheManager cm = CacheManagerBuilder
+        CacheManager manager = CacheManagerBuilder
                 .newCacheManagerBuilder()
                 .withCache(
                         "preConfigured", // 缓存别名
@@ -80,29 +85,27 @@ public class EhcacheTests {
                         )
                 )
                 .build();
-        cm.init();
+        manager.init();
 
         // 直接从缓存管理器创建一个新的缓存
-        Cache<Integer, String> myCache = cm.createCache(
-                "myCache",
+        Cache<Long, String> cache = manager.createCache(
+                "cache_1",
                 CacheConfigurationBuilder.newCacheConfigurationBuilder(
-                        Integer.class,
+                        Long.class,
                         String.class,
                         ResourcePoolsBuilder.heap(10)
                 ).build()
         );
 
         // 向缓存里添加缓存键值
-        for (int i = 0; i < 20; i++) {
-            myCache.put(i, String.format("@%d", i));
-        }
+        for (long i = 0; i < 20; i++)
+            cache.put(i, String.format("@%d", i));
 
         // 从指定缓存里获取键值
-        for (int i = 0; i < 20; i++) {
-            logger.info(String.format("%d : %s", i, myCache.get(i)));
-        }
+        for (long i = 0; i < 20; i++)
+            System.out.println(String.format("%d : %s", i, cache.get(i)));
 
-        cm.close();
+        manager.close();
     }
 
     @Test
@@ -117,7 +120,7 @@ public class EhcacheTests {
             userManagedCache.put(i, String.format("#%d", i));
             // 读
             String val = userManagedCache.get(i);
-            logger.info(String.format("%d : %s", i, val));
+            System.out.println(String.format("%d : %s", i, val));
         }
 
         userManagedCache.close();
@@ -141,14 +144,14 @@ public class EhcacheTests {
         Cache<Integer, String> threeTieredCache = persistentCacheManager.getCache("threeTieredCache", Integer.class, String.class);
 
         // 写
-        for (int i = 0; i <= 200000; i++){
+        for (int i = 0; i <= 200000; i++) {
             threeTieredCache.put(i, String.format("$%d", i));
         }
 
         // 读
-        for (int i = 0; i <= 200000; i++){
+        for (int i = 0; i <= 200000; i++) {
             String value = threeTieredCache.get(i);
-            logger.info(String.format("[R] %d : %s", i, value));
+            System.out.println(String.format("[R] %d : %s", i, value));
         }
 
         persistentCacheManager.close();
@@ -158,13 +161,15 @@ public class EhcacheTests {
     @Test
     public void test5() {
         String path = this.getClass().getResource("/").getPath();
-        logger.info("----" + path );
+        System.out.println("----" + path );
         // 对于磁盘层，数据存储在磁盘上。磁盘的速度越快、越专注，访问数据的速度就越快。
         // 要获得一个PersistentCacheManager,其实就是一个普通的通常的CacheManager，但是具有销毁缓存的能力。
-        PersistentCacheManager persistentCacheManager = CacheManagerBuilder.newCacheManagerBuilder()
-                .with(CacheManagerBuilder.persistence(new File(path, "myData"))) //提供一个存储数据的位置
+        PersistentCacheManager manager = CacheManagerBuilder.newCacheManagerBuilder()
+                .with(CacheManagerBuilder.persistence(new File(path, "myData"))) // 提供一个存储数据的位置
                 .withCache("cache_1",
-                        CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class,
+                        CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                                Long.class,
+                                String.class,
                                 // 为缓存使用的磁盘定义一个资源池。第三个参数是一个布尔值，用于设置磁盘池是否持久。
                                 // 当设置为true时，池是持久的。当使用两个参数磁盘(long、MemoryUnit)的版本时，池就不会持久。
                                 ResourcePoolsBuilder.newResourcePoolsBuilder()
@@ -177,11 +182,11 @@ public class EhcacheTests {
                 )
                 .build(true);
 
-        Cache<Long, String> cache = persistentCacheManager.getCache("cache_1", Long.class, String.class);
+        Cache<Long, String> cache = manager.getCache("cache_1", Long.class, String.class);
         //存储在缓存中的所有值都将在JVM重新启动后可用(假定CacheManager通过调用close()已被干净地关闭了。
         cache.put(1L, "stillAvailableAfterRestart");
-        logger.info(String.format("cache, 获取信息：%s", cache.get(1L)));
-        persistentCacheManager.close();
+        System.out.println(String.format("从cache获取信息：%s", cache.get(1L)));
+        manager.close();
         //上面的示例分配了非常少量的磁盘存储。您通常会使用更大的存储空间。
         //持久性意味着缓存将在JVM重新启动后存活。在重新启动JVM并在相同的位置创建一个CacheManager磁盘持久性之后，缓存中的所有内容仍然存在。
         //磁盘层不能在缓存管理器之间共享。一个持久性目录是专门针对一个缓存管理器的。
@@ -189,6 +194,47 @@ public class EhcacheTests {
         //* 你有大量的数据不适合堆在堆里
         //* 你的磁盘比它缓存的存储快得多
         //* 你对持久性感兴趣
+    }
+
+    @Test
+    public void test6() {
+        String path = this.getClass().getResource("/").getPath();
+        System.out.println("----" + path );
+        PersistentCacheManager manager = CacheManagerBuilder.newCacheManagerBuilder()
+                .with(CacheManagerBuilder.persistence(new File(path, "myData"))) // 提供一个存储数据的位置
+                .withCache("cache_1",
+                        CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                                Long.class,
+                                String.class,
+                                ResourcePoolsBuilder.newResourcePoolsBuilder()
+                                        .heap(10, EntryUnit.ENTRIES)
+                                        .disk(10, MemoryUnit.MB, true))
+                )
+                .build(true);
+
+        Cache<Long, String> cache = manager.getCache("cache_1", Long.class, String.class);
+        //cache.put(1L, "stillAvailableAfterRestart");
+        System.out.println(String.format("从cache获取信息：%s", cache.get(1L)));
+        manager.close();
+    }
+
+    // 通过xml配置
+    @Test
+    public void test7() {
+        URL url = getClass().getResource("/ehcache.xml");
+        Configuration xmlConf = new XmlConfiguration(url);
+        CacheManager manager = CacheManagerBuilder.newCacheManager(xmlConf);
+        manager.init();
+        Cache<Long, String> cache = manager.getCache("cache_1", Long.class, String.class);
+        cache.put(1L, "Hello world");
+        System.out.println(cache.get(1L));
+
+        cache.clear();
+        manager.close();
+    }
+
+    @Test
+    public void test7_1() {
     }
 
     private static String getStoragePath() {
